85f17d611656406469b82cdaa4b9f380bedace20
sahil <preetsahil289@gmail.com>
2023-12-30 02:25:26 +0530

    made some changes to footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,10 +1,7 @@
 import React from "react";
-import {
-  AiOutlineFacebook,
-  AiOutlineInstagram,
-  AiOutlineMail,
-  AiOutlineTwitter,
-} from "react-icons/ai";
+import { AiOutlineInstagram, AiOutlineMail } from "react-icons/ai";
+import { RiTwitterXLine } from "react-icons/ri";
+import { PiLinkedinLogo } from "react-icons/pi";
 import "./Footer.scss";
 import creditCardImg from "../../assets/creditcardicons.png";
 
@@ -16,16 +13,32 @@ function Footer() {
           <div className="footer-left">
             <h3 className="title">Follow us</h3>
             <ul className="follow">
-              <li className="hover-link center">
+              <li
+                className="hover-link center"
+                onClick={() =>
+                  window.open("https://www.instagram.com/_sahil217/")
+                }
+              >
                 <AiOutlineInstagram />
               </li>
-              <li className="hover-link center">
-                <AiOutlineFacebook />
+              <li
+                className="hover-link center"
+                onClick={() =>
+                  window.open("https://www.linkedin.com/in/preet-sahil/")
+                }
+              >
+                <PiLinkedinLogo />
               </li>
-              <li className="hover-link center">
-                <AiOutlineTwitter />
+              <li
+                className="hover-link center"
+                onClick={() => window.open("https://twitter.com/sahil21_7")}
+              >
+                <RiTwitterXLine />
               </li>
-              <li className="hover-link center">
+              <li
+                className="hover-link center"
+                onClick={() => window.open("mailto:preetsahil289@gmail.com")}
+              >
                 <AiOutlineMail />
               </li>
             </ul>
@@ -45,7 +58,7 @@ function Footer() {
           <div className="credit-card-img">
             <img src={creditCardImg} alt="credit card img" />
           </div>
-          <p style={{marginTop:'5px'}}>
+          <p style={{ marginTop: "5px" }}>
             Copyright {new Date().getFullYear()} © <strong>PosterVista.</strong>
           </p>
         </div>
